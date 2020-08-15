9ba9b31cff78a942c97001672ea61ff307bb5cf0
Mario Llobet <mario.llobet@onapsis.com>
2020-08-15 15:26:13 -0300

    working version with floating IP

--- T2/entrypoint.sh
+++ T2/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-/usr/sbin/keepalived -n -l -D -f /etc/keepalived/keepalived.conf --dont-fork --log-console &
+# /usr/sbin/keepalived -n -l -D -f /etc/keepalived/keepalived.conf --dont-fork --log-console &
+/usr/sbin/keepalived -n -l -D -f /etc/keepalived/keepalived.conf --dont-fork --log-console
 
-nginx -g "daemon off;"
+
+# nginx -g "daemon off;"
--- T2/states.sh
+++ T2/states.sh
@@ -6,12 +6,15 @@ STATE=$3
 
 case $STATE in
         "MASTER") echo "master"
+                  nginx -g "daemon off;"
                   exit 0
                   ;;
         "BACKUP") echo "bkp"
+                  #nginx -s stop
                   exit 0
                   ;;
         "FAULT")  echo "fault"
+                  nginx -s stop
                   exit 0
                   ;;
         *)        echo "unknown state"

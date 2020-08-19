715fe5e02f038fe915f1bb5d86f74025c282ef15
Mario Llobet <mario.llobet@onapsis.com>
2020-08-18 22:39:01 -0300

    - added named groups to search - stream downloading - Download percentage - Tempfile -re.search instead findall.

--- T6/script.py
+++ T6/script.py
@@ -6,20 +6,23 @@ import urllib3
 import time
 import gzip
 import re
+from urllib3.exceptions import ReadTimeoutError
+import tempfile
 
 # Configs
 BASE_URL = 'https://dumps.wikimedia.org/other/pagecounts-raw/2016/2016-01/'
 FILENAME_HEAD = 'pagecounts-'
-READ_INDEX_TIMEOUT = 5
-READ_TIMEOUT = 120
+READ_TIMEOUT = 30
 CONNECT_TIMEOUT = 10
 
-http = urllib3.PoolManager()
+ONE_MEG = 2**20
+CHUNK_SIZE = ONE_MEG//2
+
+http = urllib3.PoolManager(timeout=urllib3.Timeout(connect=CONNECT_TIMEOUT, read=READ_TIMEOUT))
 
 status = None
 while status != 200:
-    r = http.request('GET', BASE_URL,
-                     timeout=urllib3.Timeout(connect=CONNECT_TIMEOUT, read=READ_INDEX_TIMEOUT))
+    r = http.request('GET', BASE_URL)
     status = r.status
     time.sleep(2)
 
@@ -30,29 +33,46 @@ soup = BeautifulSoup(r.data, features='html.parser')
 pg_files = [f['href'] for f in soup.find_all ('a', href=True) if f['href'].startswith(FILENAME_HEAD)]
 print(f'{len(pg_files)} files were found in: {BASE_URL}')
 
-regex = re.compile(r'en\ Bitcoin [0-9]+')
-regex_hits = []
+# Regex named groups
+regex = re.compile(r'(?P<domain>en) (?P<term>[Bb]itcoin) (?P<hits_number>\d+)')
+acc_hits = 0
 
 for pg_file in pg_files:
 
     # Compose File URL
     FILE_URL = BASE_URL + pg_file
     # Get remote file data
-    file_data = http.request('GET', FILE_URL, preload_content=False,
-                             timeout=urllib3.Timeout(connect=CONNECT_TIMEOUT, read=READ_TIMEOUT))
+    r = http.request('GET', FILE_URL, preload_content=False)
+    # Get file size.
+    content_bytes = int(r.headers.get("Content-Length"))
 
-    with gzip.open(file_data, 'rb') as f:
-        file_content = f.read().decode("utf-8")
+    print(f'Downloading and Unzipping: {FILE_URL}')
 
-    try:
-        find = re.findall(regex, file_content)[0]
-        hits = int(find.split(' ')[2])
-        # append the finding to a list as int.
-        regex_hits.append(hits)
-        print(f'Match: {find}')
-        print(f'{pg_file} has {hits} hits...')
-        print(f'Accumulated hits: {sum(regex_hits)}')
+    chunks = 0
+    with tempfile.TemporaryFile(mode='w+b') as fp:
+        for chunk in r.stream(CHUNK_SIZE):
+            print(f'Downloaded: {((CHUNK_SIZE * chunks) / content_bytes) * 100:.1f}%')
+            fp.write(chunk)
+            chunks += 1
+            
+        r.release_conn()
+        fp.seek(0)
 
-    except IndexError:
-        print(f'No findings for file {pg_file}...')
-        print(f'Accumulated hits: {sum(regex_hits)}')
+        # Opened as text (rt) no decoding needed.
+        with gzip.open(fp, 'rt') as f:
+            file_content = f.read()
+        
+        print(f'Gzip file opened - searching a regex match')
+
+        try:
+            find = regex.search(file_content)
+            hits = int(find.group('hits_number'))
+
+            acc_hits += hits
+            print(f'Match: {find.group(0)}')
+            print(f'{pg_file} has {hits} hits.')
+            print(f'Accumulated hits: {acc_hits}')
+
+        except AttributeError:
+            print(f'No findings for file {pg_file}.')
+            print(f'Accumulated hits: {acc_hits}')
